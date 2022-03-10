47225e6303af4f9a8921f7280d58bb3fd04c89cf
Eniola Anthony <eniolaanthony@gmail.com>
2022-03-10 10:59:15 +0600

    Feat: add tableView to SecondViewController

--- RxswiftBuild/SecondViewController.swift
+++ RxswiftBuild/SecondViewController.swift
@@ -9,29 +9,49 @@ import UIKit
 
 class SecondViewController: UIViewController, Coordinating {
   var coordinator: Coordinator?
-  let label: UILabel = {
-    let label = UILabel()
-    label.text = "Test!!"
-    label.textColor = .black
-    return label
-    
-  }()
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view.
-    view.backgroundColor = .white
-    title = "SecondViewController"
-    view.addSubview(label)
-    configureView()
-  }
+  
+  private  let tableview: UITableView = {
+       let table = UITableView(frame: .zero, style: .grouped)
+       table.register(UITableViewCell.self, forCellReuseIdentifier: "tony")
+       return table
+   }()
+   
+   private let models = [
+       "Model S",
+       "Model X",
+       "Model Y",
+       "Model 3",
+       "Model S P90"
+   ]
+   
+   override func viewDidLoad() {
+       super.viewDidLoad()
+       // Do any additional setup after loading the view.
+       view.addSubview(tableview)
+       tableview.dataSource = self
+   }
 
-  func configureView() {
-    label.snp.makeConstraints { make in
-      make.centerX.equalToSuperview()
-      make.centerY.equalToSuperview()
-    }
-  }
-  
-  
+   override func viewDidLayoutSubviews() {
+       super.viewDidLayoutSubviews()
+       tableview.frame = view.bounds
+   }
 
 }
+
+extension SecondViewController: UITableViewDataSource {
+  func numberOfSections(in tableView: UITableView) -> Int {
+      return 2
+  }
+
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+   return  models.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      let cell = tableView.dequeueReusableCell(withIdentifier: "tony", for: indexPath)
+      cell.textLabel?.text = models[indexPath.row]
+      return cell
+  }
+  
+}
+
